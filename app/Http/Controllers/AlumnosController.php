4a56d3a32822ac59a07d2c27ca9f39d5bde9e2a8
Alumno ULSA <alumno@D521.local>
2023-03-01 14:29:42 -0700

    Mensaje

---
+++ app/Http/Controllers/AlumnosController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Alumno;
+
+class AlumnosController extends Controller
+{
+    public function index() {
+        $alumnos = Alumno::all();
+
+        $argumentos = array();
+        $argumentos['alumnos'] = $alumnos;
+
+        return view('alumnos.index',$argumentos);
+    }
+    public function create() {
+        $argumentos = array();
+        return view('alumnos.create', $argumentos);
+    }
+    public function store(Request $request) {
+        $nuevoAlumno = new Alumno();
+        //Las columnas de la tabla asociada 
+        //se representan mediante propiedades del objeto
+        $nuevoAlumno->nombre = $request->input('nombre');
+        $nuevoAlumno->save();
+        //hacer que nos regrese al index
+        return redirect()->route('alumnos.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlumnosController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//>
+
+//Index - Listar
+Route::get('/alumnos',[AlumnosController::class,'index'])
+    ->name('alumnos.index');
+//Create - Formulario de creacion
+Route::get('/alumnos/create',[AlumnosController::class,'create'])
+    ->name('alumnos.create');
+//Store - almacenar un nuevo registro
+Route::post('/alumnos',[AlumnosController::class,'store'])
+    ->name('alumnos.store');
